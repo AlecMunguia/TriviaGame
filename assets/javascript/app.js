7679ab92d1ba592975341c54ee6f2fa8a8a40fe1
Spinning Plates <alec.munguia@gmail.com>
2019-04-06 18:39:19 -0500

    finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
+   //create quiz data
+    var questions = [
+        {   question:   "What macro does the keto diet restrict?",
+            answers:    ["Protein", "Carbohydrates", "Fiber"],
+            correct:    "Carbohydrates"
+        },
+       {    question:   "What is released by the liver during Ketosis?",
+            answers:    ["Ketones", "Exogenous Ketones", "Creatine"],
+            correct:    "Ketones"
+        },
+        {   question:   "Which of these foods is permitted under the Ketogenic diet?",
+            answers:    ["Bananas", "Whole-Grain Bread", "Bacon"],
+            correct:    "Bacon"
+        },
+        {   question:   "Which of these foods is NOT permitted under the Ketogenic diet?",
+            answers:    ["Rice", "Kimchi", "Cream Cheese"],
+            correct:    "Rice" 
+        }
+    ];
+    //display questions and answers
+   for (var i = 0; i < questions.length; i++) {
+       $("#questionBox").append("<div>"+questions[i].question + "</div>");
+        for (var j = 0; j < questions[i].answers.length; j++ ) {
+            $("#questionBox").append('<input type = "radio" name = "option'+i+'" value = "'+questions[i].answers[j]+'">'+questions[i].answers[j])
+        }
+} 
     //count the seconds past
 var counter = 0;
     //set total time
@@ -15,71 +41,39 @@ timer.html(convertSeconds('0'));
     //make timeLeft - seconds past
 function timeIt() {
     counter++
-    timer.html(convertSeconds(timeLeft - counter));  
+    timer.html(convertSeconds(timeLeft - counter)); 
+    //stop timer at 0 then check answers and display results
+    if (counter === 60) {
+        clearInterval (intervalID);
+        checkAnswers();
+        alert ("You answered " + correct + " out of 4 correctly");
+        correct = 0;
+    } 
 }
-setInterval(timeIt, 1000);
-
-    // NEED TIMER TO STOP AT '0' AND DISPLAY SCORE ALERT //
-
-
+var intervalID = setInterval(timeIt, 1000);
+    //when the button is clicked stop counter check answers and display results
 $('#button').click(function () {
+    clearInterval (intervalID);
+    checkAnswers();
     alert ("You answered " + correct + " out of 4 correctly");
+    correct = 0;
 });
-
-    // NEED TO COUNT NUMBER OF CORRECT / INCORRECT ANSWERS
-
+    //count correct answers
 var correct = 0;
 
-var questions = [
-    {   question:   "What macro does the keto diet restrict?",
-        choiceA:    "Protein",
-        choiceB:    "Carbohydrates",
-        choiceC:    "Fiber",
-        correct:    "B"
-    },
-    {   question:   "What is released by the liver during Ketosis?",
-        choiceA:    "Ketones",
-        choiceB:    "Exogenous Ketones",
-        choiceC:    "Creatine",
-        correct:    "A"
-    },
-    {   question:   "Which of these foods is permitted under the Ketogenic diet?",
-        choiceA:    "Bananas",
-        choiceB:    "Whole-Grain Bread",
-        choiceC:    "Bacon",
-        correct:    "C"
-    },
-    {   question:   "Which of these foods is NOT permitted under the Ketogenic diet?",
-        choiceA:    "Rice",
-        choiceB:    "Kimchi",
-        choiceC:    "Cream Cheese",
-        correct:    "A"        
-}];
-
-$("#first-question").text(questions[0].question);
-$("#second-question").text(questions[1].question);
-$("#third-question").text(questions[2].question);
-$("#fourth-question").text(questions[3].question);
-
-
-if(document.getElementById('radio1').checked) {
-} else if(document.getElementById('radio2').checked) {
-} else {
-  return false;
-}
-
-
-
-        // TIMER NOTES
-// start timer at 60 seconds
-// have counter decrease by one every second
-// when counter hits '0' have it stop and display 'score'
-
-        // GAME NOTES
-// display the 4 questions
-// give 3 possible answers with buttons
-// make only one button selectable at a time
-// log correct and incorrect 
-// create submit button that pushes the counter to '0'
-// when counter hits '0' display results
+    //check answers
+function checkAnswers () {
+    if ($("input[name=option1]:checked").val() == questions[1].correct) {
+        correct++;
+        }
+    if ($("input[name=option2]:checked").val() == questions[2].correct) {
+        correct++;
+        }
+    if ($("input[name=option3]:checked").val() == questions[3].correct) {
+        correct++;
+        } 
+    if ($("input[name=option0]:checked").val() == questions[0].correct) {
+        correct++;
+        }
+    }
 });
